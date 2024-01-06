a0ba04973bbd2a70e4b0ea72e9433b5354219606
Raj Sharma <raj.sharma@phonepe.com>
2024-01-06 21:07:35 +0530

    bruh

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from "./(home)/header";
-
-export default function Home() {
-  return (
-    <main className="flex items-center h-screen justify-center">
-      <Header />
-    </main>
-  );
-}
